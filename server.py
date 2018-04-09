887b395a4db18de1299694fd1e0e280a051bdd39
Juan Benitez <juanbenitez2908@gmail.com>
2018-04-08 20:06:34 -0500

    Agregada Static Folder

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import request, render_template
 
 import serial_python as serial
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route("/")
@@ -13,17 +13,10 @@ def hello():
         num = 1
     return str(num)
 
-@app.route('/sketch.js')
-def sketch():
-    return render_template('scripts/sketch.js')
-
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/p5.min.js')
-def p5():
-    return render_template('scripts/p5.min.js')    
 
 if __name__ == '__main__':
     app.run("localhost", 8080)
